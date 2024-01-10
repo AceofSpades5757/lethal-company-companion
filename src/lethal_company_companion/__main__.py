ba1606a16e046ad1dfe89399443105ba8679fbea
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2024-01-09 23:00:08 -0600

    ci: run

--- src/lethal_company_companion/__main__.py
+++ src/lethal_company_companion/__main__.py
@@ -10,33 +10,34 @@ TODO: Refactor to a CLI tool.
 """
 import logging
 import os
-from typing import Final, Optional
-from typing import NoReturn
-from typing import Callable
-from typing import Annotated
 from dataclasses import dataclass
 from dataclasses import field
+from typing import Annotated
+from typing import Callable
+from typing import Final
+from typing import NoReturn
+from typing import Optional
 
 import keyboard
 import typer
-
 
 LOOP_DELAY: Final[float] = 0.1  # seconds
 
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    datefmt='%d-%b-%y %H:%M:%S',
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
     handlers=[
         logging.StreamHandler(),
-    ]
+    ],
 )
 
 
 @dataclass
 class Macro:
     """Macro class."""
+
     name: str
     keys: tuple[str, ...]
     help_text: str
@@ -47,7 +48,7 @@ class Macro:
         # print(f'Press {SWITCH_KEY} to run "switch" macro.')
         # Press F1, F2, or F3 to run <NAME> macro: <HELP_TEXT>
         print(f'Press {self.keys} to run "{self.name}" macro:')
-        print(f'    {self.help_text}')
+        print(f"    {self.help_text}")
 
     def set_hotkeys(self) -> None:
         """Set hotkeys."""
@@ -55,10 +56,10 @@ class Macro:
             keyboard.add_hotkey(key, self.function)
 
 
-
 @dataclass
 class Settings:
     """Settings class."""
+
     debug: bool = False
     radar_booster_name: Optional[str] = None
     macros: list[Macro] = field(default_factory=list)
@@ -66,9 +67,9 @@ class Settings:
 
 def flash(radar_booster: str) -> None:
     """Type out flash macro."""
-    keyboard.write('flash ')
+    keyboard.write("flash ")
     keyboard.write(radar_booster)
-    keyboard.press_and_release('enter')
+    keyboard.press_and_release("enter")
     # guard against flashlight purchases
     # WARNING: This causes an annoying sound to be played, every time.
     # time.sleep(0.1)
@@ -78,29 +79,31 @@ def flash(radar_booster: str) -> None:
 
 def ping(radar_booster_name: str) -> None:
     """Type out ping macro."""
-    keyboard.write('ping ')
+    keyboard.write("ping ")
     keyboard.write(radar_booster_name)
-    keyboard.press_and_release('enter')
+    keyboard.press_and_release("enter")
 
 
 def switch() -> None:
     """Type out switch macro."""
-    keyboard.write('switch')
-    keyboard.press_and_release('enter')
+    keyboard.write("switch")
+    keyboard.press_and_release("enter")
 
 
 def view_monitor() -> None:
     """Toggle monitor view."""
-    keyboard.write('view monitor')
-    keyboard.press_and_release('enter')
+    keyboard.write("view monitor")
+    keyboard.press_and_release("enter")
 
 
 def toggle_debug(settings: Settings) -> None:
     """Toggle debug."""
     settings.debug = not settings.debug
-    logging.getLogger().setLevel(logging.DEBUG if settings.debug else logging.INFO)
+    logging.getLogger().setLevel(
+        logging.DEBUG if settings.debug else logging.INFO
+    )
 
-    os.system('clear')
+    os.system("clear")
     print(f'DEBUG: {"ON" if settings.debug else "OFF"}')
 
 
@@ -112,42 +115,50 @@ def main(
     macros: list[Macro] = [
         # System
         Macro(
-            name='quit',
-            keys=('F12',),
-            help_text='Quit the program.',
+            name="quit",
+            keys=("F12",),
+            help_text="Quit the program.",
             function=lambda: os._exit(0),
         ),
         Macro(
-            name='debug',
-            keys=('F11',),
-            help_text='Toggle debugging.',
+            name="debug",
+            keys=("F11",),
+            help_text="Toggle debugging.",
             function=lambda: toggle_debug(settings),
         ),
         # Basic
         Macro(
-            name='view monitor',
-            keys=('F5',),
-            help_text='Toggle monitor view.',
+            name="view monitor",
+            keys=("F5",),
+            help_text="Toggle monitor view.",
             function=view_monitor,
         ),
         Macro(
-            name='switch',
-            keys=('F1',),
-            help_text='Type out switch macro.',
+            name="switch",
+            keys=("F1",),
+            help_text="Type out switch macro.",
             function=switch,
         ),
         # Radar Booster
         Macro(
-            name='flash',
-            keys=('F2',),
-            help_text='Type out flash macro.' if radar_booster_name else 'No radar booster name provided.',
-            function=lambda: flash(radar_booster_name) if radar_booster_name else None,
+            name="flash",
+            keys=("F2",),
+            help_text="Type out flash macro."
+            if radar_booster_name
+            else "No radar booster name provided.",
+            function=lambda: flash(radar_booster_name)
+            if radar_booster_name
+            else None,
         ),
         Macro(
-            name='ping',
-            keys=('F3',),
-            help_text='Type out ping macro.' if radar_booster_name else 'No radar booster name provided.',
-            function=lambda: ping(radar_booster_name) if radar_booster_name else None,
+            name="ping",
+            keys=("F3",),
+            help_text="Type out ping macro."
+            if radar_booster_name
+            else "No radar booster name provided.",
+            function=lambda: ping(radar_booster_name)
+            if radar_booster_name
+            else None,
         ),
     ]
     settings = Settings(
@@ -177,7 +188,7 @@ def main(
         macro.print_help()
         print()
 
-    input('Press ENTER to quit.\n')
+    input("Press ENTER to quit.\n")
     os._exit(0)
 
 
@@ -198,7 +209,7 @@ def run(
 
 
 @app.command(
-    name='help',
+    name="help",
 )
 def help_() -> None:
     """Print help text."""
@@ -207,5 +218,5 @@ def help_() -> None:
     typer.echo(ctx.get_help())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     raise SystemExit(main())
