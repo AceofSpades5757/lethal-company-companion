e3b9d3da716cb161f86f69950c5b8688f7f62928
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2024-02-01 00:08:06 -0600

    feat: add gui

---
+++ src/lcc/gui.py
@@ -0,0 +1,22 @@
+"""GUI
+
+https://www.pythonguis.com/courses/pyside-getting-started/
+https://doc.qt.io/qtforpython-6/quickstart.html#quick-start
+"""
+import PySide6.QtCore
+
+
+# Prints PySide6 version
+print(PySide6.__version__)
+
+# Prints the Qt version used to compile PySide6
+print(PySide6.QtCore.__version__)
+
+
+def app() -> None:
+    # TODO: implement
+    pass
+
+
+if __name__ == "__main__":
+    app()
